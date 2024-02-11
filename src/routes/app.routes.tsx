8500d10e7de7e2ab32dbc92b22ab28b4d2be21e4
Henrique Marques <hmdarkfire.dev@gmail.com>
2024-02-11 14:36:08 -0300

    chore: config navigation

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export const AppRoutes = () => {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="Home" component={Home} />
+    </Navigator>
+  );
+};
