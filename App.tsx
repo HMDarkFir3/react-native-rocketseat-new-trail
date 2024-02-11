2536d2876fe5d3780f960ffc107a585b20b7ed00
Henrique Marques <hmdarkfire.dev@gmail.com>
2024-02-11 14:38:15 -0300

    chore: config gesture handler

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
@@ -19,9 +20,11 @@ const App = () => {
   SplashScreen.hideAsync();
 
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
