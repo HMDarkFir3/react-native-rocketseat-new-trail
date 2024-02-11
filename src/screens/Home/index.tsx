fe616c23a276e5b6523a87843ebb6f729a6e20e6
Henrique Marques <hmdarkfire.dev@gmail.com>
2024-02-11 15:52:37 -0300

    feat: task dto

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,6 +5,8 @@ import { Input } from '@/components/Input';
 import { SmallButton } from '@/components/SmallButton';
 import { QuantityCard } from '@/components/QuantityCard';
 
+import { TaskDTO } from '@/dtos/TaskDTO';
+
 import Logo from '../../assets/logo.svg';
 
 import {
@@ -17,6 +19,18 @@ import {
 
 export const Home = () => {
   const [task, setTask] = useState<string>('');
+  const [tasks, setTasks] = useState<TaskDTO[]>([
+    {
+      id: '1',
+      name: 'Fazer café',
+      done: false,
+    },
+    {
+      id: '2',
+      name: 'Fazer café',
+      done: false,
+    },
+  ]);
 
   return (
     <Container>
@@ -35,7 +49,7 @@ export const Home = () => {
         </Floating>
 
         <QuantityWrapper>
-          <QuantityCard title="Criadas" quantity={0} type="create" />
+          <QuantityCard title="Criadas" quantity={tasks.length} type="create" />
           <QuantityCard title="Concluídas" quantity={0} type="done" />
         </QuantityWrapper>
       </Wrapper>
