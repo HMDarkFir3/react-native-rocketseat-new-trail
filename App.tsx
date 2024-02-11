a904edad4671a05407f0a0986bbf900959e49d4b
Henrique Marques <hmdarkfire.dev@gmail.com>
2024-02-10 21:07:08 -0300

    feat: configure font and prevent splash

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from '@/routes';
 
 import { dark } from '@/themes/dark';
 
 const App = () => {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
     <NavigationContainer>
       <ThemeProvider theme={dark}>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,7 +5,7 @@ import { Container, Title } from './styles';
 export const Home = () => {
   return (
     <Container>
-      <Title>Home</Title>
+      <Title>Rocketseat</Title>
     </Container>
   );
 };
